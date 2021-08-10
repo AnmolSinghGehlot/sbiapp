c221bf65f2797bbc251c40896118fcba16cd69f6
hp <hp@LAPTOP-05P0008S>
2021-08-10 14:23:14 +0530

    Final

--- src/main/java/com/example/SbibankappApplication.java
+++ src/main/java/com/example/SbibankappApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbibankApplication {
+public class SbibankappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbibankApplication.class, args);
+		SpringApplication.run(SbibankappApplication.class, args);
 	}
 
 }
