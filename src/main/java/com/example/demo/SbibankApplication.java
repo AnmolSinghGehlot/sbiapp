8aecaec7a49bb51b266eec56190d074bfccfa68d
hp <hp@LAPTOP-05P0008S>
2021-08-10 12:54:54 +0530

    first commit

---
+++ src/main/java/com/example/demo/SbibankApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbibankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbibankApplication.class, args);
+	}
+
+}
